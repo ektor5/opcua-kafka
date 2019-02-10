324b180e04c3ec774e489d45a96271d79a11966e
Ettore Chimenti <ek5.chimenti@gmail.com>
2019-02-10 22:04:50 +0100

    Using newer versions

--- client/client.js
+++ client/client.js
@@ -1,76 +1,106 @@
 var opcua = require("node-opcua");
 var async = require("async");
 
-var kafka = require('kafka-node');
-var Producer = kafka.Producer;
-var client = new kafka.Client('localhost:2181');
-var producer = new Producer(client);
+const kafka = require('kafka-node');
+const kclient = new kafka.KafkaClient({kafkaHost: '192.168.0.102:9092'});
+const Producer = kafka.Producer;
+const producer = new Producer(kclient);
 
 producer.on('ready', function(){console.log('Connected to Kafka!');});
   
-var client = new opcua.OPCUAClient();
-var endpointUrl = "opc.tcp://" + require("os").hostname().toLowerCase() + ":4334/UA/SampleServer";
+
+//var endpointUrl = "opc.tcp://" + require("os").hostname().toLowerCase() + ":4334/UA/SampleServer";
+var endpointUrl = "opc.tcp://" + require("os").hostname().toLowerCase() + ":4334";
 
 var session, subscription;
 var tag = process.argv[2];
 
+
+const securityMode = opcua.MessageSecurityMode.get("NONE");
+if (!securityMode) {
+    throw new Error("Invalid Security mode , should be " + opcua.MessageSecurityMode.enums.join(" "));
+}
+
+const securityPolicy = opcua.SecurityPolicy.get("None");
+if (!securityPolicy) {
+    throw new Error("Invalid securityPolicy , should be " + opcua.SecurityPolicy.enums.join(" "));
+}
+const options = {
+	securityMode: securityMode,
+	securityPolicy: securityPolicy,
+	endpoint_must_exist: false,
+	keepSessionAlive: true,
+	connectionStrategy: {
+		maxRetry: 10,
+		initialDelay: 2000,
+		maxDelay: 10*1000
+	}
+};
+
+const client = new opcua.OPCUAClient(options);
+
+
+console.log('ok');
 async.series([
-    // step 1 : connect to
-    function(callback)  {
-      client.connect(endpointUrl,function (err) {
-          if(err) { console.log(" cannot connect to endpoint :" , endpointUrl ); }
-          else { console.log("connected !"); }
-          callback(err);
-      });
-    },
+	function(callback)  {
+		// step 1 : connect to
+		client.connect(endpointUrl,function (err) {
+			if(err) { console.log(" cannot connect to endpoint :" , endpointUrl ); }
+			else { console.log("connected !"); }
+			callback(err);
+		});
+	},
 
-    // step 2 : createSession
-    function(callback) {
-      client.createSession( function(err, _session) {
-          if(!err) { session = _session; }
-          callback(err);
-      });
-    },
+	// step 2 : createSession
+	function(callback) {
+		let userIdentity = null;
+		client.createSession(function(err, _session) {
+			if(!err) { session = _session; }
+			callback(err);
+		});
+	},
 
-    // step 5: install a subscription and install a monitored item for 10 seconds
-    function(callback) {
-      subscription=new opcua.ClientSubscription(session,{
-          requestedPublishingInterval: 1000,
-          requestedLifetimeCount: 10,
-          requestedMaxKeepAliveCount: 2,
-          maxNotificationsPerPublish: 10,
-          publishingEnabled: true,
-          priority: 10
-      });
+	// step 5: install a subscription and install a monitored item for 10 seconds
+	function(callback) {
+		subscription=new opcua.ClientSubscription(session,{
+			requestedPublishingInterval: 1000,
+			requestedLifetimeCount: 10,
+			requestedMaxKeepAliveCount: 2,
+			maxNotificationsPerPublish: 10,
+			publishingEnabled: true,
+			priority: 10
+		});
 
-      subscription.on("started",function(){
-        console.log("subscriptionId=", subscription.subscriptionId);
-      }).on("keepalive",function(){ console.log(); })
-      .on("terminated",function(){ callback(); });
+		subscription.on("started",function(){
+			console.log("subscriptionId=", subscription.subscriptionId);
+		}).on("keepalive",function(){ console.log("ka"); })
+			.on("terminated",function(){ callback(); });
 
-      // install monitored item
-      var monitoredItem  = subscription.monitor(
-        {nodeId: opcua.resolveNodeId(tag), attributeId: 13},
-        {samplingInterval: 100, discardOldest: true, queueSize: 10}
-      );
-      console.log("-------------------------------------");
+		console.log(opcua.AttributeIds.Value );
+		// install monitored item
+		var monitoredItem  = subscription.monitor(
+			{nodeId: opcua.resolveNodeId(tag), attributeId: opcua.AttributeIds.Value },
+			{samplingInterval: 100, discardOldest: true, queueSize: 10},
+		);
+		console.log("-------------------------------------");
 
-      monitoredItem.on("changed",function(dataValue){
-         console.log("ChangedVal:", JSON.stringify(dataValue));
-         producer.send([
-         {topic: 'test',
-          messages:[JSON.stringify(dataValue)]
-         }], function(err, data){
-           if (err){ console.log('Error sending: ', err); }
-           else{ console.log('Successfully published: ' + new Date() + ', ' + data) }
-         });
-      });
-    }
+		monitoredItem.on("changed",function(dataValue){
+			console.log("ChangedVal:", JSON.stringify(dataValue));
+			producer.send([
+				{topic: 'test',
+					messages:[JSON.stringify(dataValue.value.value)],
+					partition: 0
+				}], function(err, data){
+					if (err){ console.log('Error sending: ', err); }
+					else{ console.log('Successfully published: ' + new Date() + ', ' + data) }
+				});
+		});
+	}
 ],
-function(err) {
-  if (err) { console.log(" failure ",err); }
-  else { console.log('done!'); }
-  client.disconnect(function(){});
-});
+	function(err) {
+		if (err) { console.log(" failure ",err); }
+		else { console.log('done!'); }
+		client.disconnect(function(){});
+	});
 
 
--- server/server.js
+++ server/server.js
@@ -8,35 +8,52 @@ var var1 = {nodeId: 'ns=4;s=hook_weight', browseName: 'hook_weight', dataType: '
 var var2 = {nodeId: 'ns=4;s=rpm', browseName: 'rpm', dataType: 'Double'};
 
 function post_initialize(){
-    console.log("server initialized");
-    function construct_my_address_space(server) {
-      server.engine.createFolder("RootFolder",{ browseName: siteName});
-        // emulate variable1 changing every 500 ms
-        var variable1 = 1;
-        setInterval(function(){ variable1 += 1; }, 1000);
+	console.log("server initialized");
+	function construct_my_address_space(server) {
+		//   server.engine.createFolder("RootFolder",{ browseName: siteName});
+		// emulate variable1 changing every 500 ms
+	}
 
-        var1.value = {
-          get: function(){
-            return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
-          }
-        };
-        server.var1 = server.engine.addVariableInFolder(siteName, var1);
+	opcua.build_address_space_for_conformance_testing(server.engine.addressSpace);
+	//opcua.install_optional_cpu_and_memory_usage_node(server);
+	const addressSpace = server.engine.addressSpace;
+	const rootFolder = addressSpace.findNode("RootFolder");
+	//assert(rootFolder.browseName.toString() === "Root");
+	const namespace = addressSpace.getOwnNamespace();
+	const myDevices = namespace.addFolder(rootFolder.objects, {browseName: siteName});
+	const Variant = opcua.Variant;
+	const DataType = opcua.DataType;
+	const DataValue = opcua.DataValue;
+	
+	let variable1 = 1;
+	setInterval(function(){ variable1 += 1; }, 1000);
 
-        var2.value = {
-          get: function(){
-            return new opcua.Variant({dataType: opcua.DataType.Double, value: 10});
-          }
-        };
-        server.var2 = server.engine.addVariableInFolder(siteName, var2);
-    }
-    
-    construct_my_address_space(server);
-    
-    server.start(function() {
-        console.log("Server is now listening ... ( press CTRL+C to stop)");
-        console.log("port ", server.endpoints[0].port);
-        var endpointUrl = server.endpoints[0].endpointDescription().endpointUrl;
-        console.log(" the primary server endpoint url is ", endpointUrl );
-    });
+	server.var1 = namespace.addVariable({
+		organizedBy: myDevices,
+		browseName: siteName,
+		nodeId: "ns=1;s=hook_weight",
+		dataType: "Double",
+		value: {
+			get: function () {
+				return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
+			}
+		}
+
+	});
+
+	server.var2 = namespace.addVariable({
+		organizedBy: myDevices,
+		browseName: siteName,
+		nodeId: "ns=1;s=rpm",
+		dataType: "Double",
+		value: new Variant({dataType: DataType.Double, value: 10})
+	});
+
+	server.start(function() {
+		console.log("Server is now listening ... ( press CTRL+C to stop)");
+		console.log("port ", server.endpoints[0].port);
+		var endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+		console.log(" the primary server endpoint url is ", endpointUrl );
+	});
 }
 server.initialize(post_initialize);
